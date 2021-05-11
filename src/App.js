7f96633032e749c9e0cfa835f0ae34e813ee5451
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-11 14:33:28 +0300

    SidebarHeader Component compolete.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <>
-          <Switch>
-             <Route path="/">
-              <Header />
-            </Route>
-          </Switch>
+          {" "}
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                {/* chat */}
+              </Route>
+            </Switch>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -19,3 +26,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
