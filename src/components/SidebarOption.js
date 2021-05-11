5436f72858bae5ae95ef52ebc5b3f317162ad16f
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-11 17:16:46 +0300

    Add a channel complete.

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { enterRoom } from "../features/appSlice";
 import { db } from "../firebase";
 
-const SidebarOption = ({ Icon, title, addChannelOption }) => {
- 
+const SidebarOption = ({ Icon, title, addChannelOption,id }) => {
+  const dispatch = useDispatch();
+
   const addChannel = () => {
     const channelName = prompt("Please enter the channel name");
 
@@ -15,7 +18,11 @@ const SidebarOption = ({ Icon, title, addChannelOption }) => {
   };
 
   const selectChannel = () => {
-    
+    if (id) {
+      dispatch(enterRoom({
+        roomId:id
+      }))
+    }
   };
 
   return (
@@ -55,7 +62,6 @@ const SidebarOptionContainer = styled.div`
   }
 `;
 const SidebarOptionChannel = styled.h3`
-  padding:10px 0;
-  font-weight:300;
-
+  padding: 10px 0;
+  font-weight: 300;
 `;
